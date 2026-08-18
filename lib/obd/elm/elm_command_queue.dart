c62fc5e5e6d847a85168d63d5e1f6f98be54688c
Unreallx <light.agma.io@gmail.com>
2026-08-18 23:15:48 +0300

    feat: add basic ELM command queue

---
+++ lib/obd/elm/elm_command_queue.dart
@@ -0,0 +1,38 @@
+import 'package:redrive/obd/elm/elm_command.dart';
+import 'package:redrive/obd/elm/elm_response.dart';
+
+class ElmCommandQueue {
+  int _nextGenerationId = 0;
+
+  ElmCommand? currentElmCommand;
+  final List<ElmCommand> waitingCommands = [];
+
+  ElmCommand incomingQueue(String command, Duration timeout) {
+    _nextGenerationId++;
+
+    ElmCommand newCommand = ElmCommand(
+      command: command,
+      timeout: timeout,
+      generationId: _nextGenerationId,
+    );
+
+    if (currentElmCommand == null) {
+      currentElmCommand = newCommand;
+    } else {
+      waitingCommands.add(newCommand);
+    }
+
+    return newCommand;
+  }
+
+  ElmCommand? completeCurrent() {
+    if (waitingCommands.isEmpty) {
+      currentElmCommand = null;
+      return null;
+    }
+
+    ElmCommand nextResponseCommand = waitingCommands.removeAt(0);
+    currentElmCommand = nextResponseCommand;
+    return nextResponseCommand;
+  }
+}
