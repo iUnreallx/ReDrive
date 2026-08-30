551089b6cec9ec409ad5e2039940b717f27d117e
Unreallx <light.agma.io@gmail.com>
2026-08-30 19:17:45 +0300

    refactor: add coolant PID metadata

--- lib/obd/pid/definitions/coolant_temperature_definition.dart
+++ lib/obd/pid/definitions/coolant_temperature_definition.dart
@@ -5,5 +5,6 @@ import '../pid_key.dart';
 final coolantTemperatureDefinition = PidDefinition<int>(
   key: PidKey.coolantTemperature,
   command: '0105',
+  pid: 0x05,
   decoder: decodeCoolantTemperature,
 );
