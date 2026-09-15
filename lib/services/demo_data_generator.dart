86b5dc3933ea71c8f58ff027cddacf75d1a1ded3
Unreallx <light.agma.io@gmail.com>
2026-09-15 23:35:01 +0300

    Update demo data generator

--- lib/services/demo_data_generator.dart
+++ lib/services/demo_data_generator.dart
@@ -25,7 +25,7 @@ class DemoDataGenerator {
     return ObdData(
       speed: 30 + _random.nextInt(270), // от 0 до 300 км/ч
       rpm: 800 + _random.nextInt(14000), // от 800 до 14799 об/мин
-      engineTemp: 30 + _random.nextInt(80), // от 30 до 109 °C
+      coolantTemp: 30 + _random.nextInt(80), // от 30 до 109 °C
       voltage: 13.0 + (_random.nextDouble() * 2.5), // от 13.0 до 15.5 V
     );
   }
