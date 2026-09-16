f9de0dfad8345ade7ecdfdbe7d22440092a1ab0b
Unreallx <light.agma.io@gmail.com>
2026-09-16 21:00:10 +0300

    docs: document OBD connection abstraction

--- lib/obd/connection/obd_connection.dart
+++ lib/obd/connection/obd_connection.dart
@@ -1,3 +1,7 @@
+/// Defines the transport interface used for OBD communication.
+///
+/// Implementations may use (Bluetooth/WiFi/Usb) or other transports while
+/// keeping the OBD layer independent from transport details.
 abstract class ObdConnection {
   Stream<String> get incoming;
 
