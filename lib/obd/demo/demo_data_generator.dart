acb01a1e4c0970ee1b1c910b9dff28441b8dba12
Unreallx <light.agma.io@gmail.com>
2026-09-20 19:28:19 +0300

    move demo data generator into OBD module

---
+++ lib/obd/demo/demo_data_generator.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'dart:math';
+import '../models/obd_data.dart';
+
+class DemoDataGenerator {
+  Timer? _timer;
+  final _random = Random();
+
+  void start(void Function(ObdData) onData) {
+    stop();
+
+    onData(_generateData());
+
+    _timer = Timer.periodic(const Duration(milliseconds: 1000), (_) {
+      onData(_generateData());
+    });
+  }
+
+  ObdData _generateData() {
+    return ObdData(
+      speed: 30 + _random.nextInt(270), // от 0 до 300 км/ч
+      rpm: 800 + _random.nextInt(14000), // от 800 до 14799 об/мин
+      coolantTemp: 30 + _random.nextInt(80), // от 30 до 109 °C
+      voltage: 13.0 + (_random.nextDouble() * 2.5), // от 13.0 до 15.5 V
+    );
+  }
+
+  void stop() {
+    _timer?.cancel();
+    _timer = null;
+  }
+}
